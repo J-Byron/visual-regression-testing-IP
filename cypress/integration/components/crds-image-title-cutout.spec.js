d1c39f77e9d9abbac1adea92bc46f68c4f2f5af8
Josh Byron <joshhbyron@gmail.com>
2020-01-26 15:45:18 -0500

    updatewq

---
+++ cypress/integration/components/crds-image-title-cutout.spec.js
@@ -0,0 +1,11 @@
+describe('Each button correctly redirects',()=>{
+
+    it('Succesfully redirects on label click',()=>{
+        cy.visit('http://localhost:8080') 
+    })
+
+    it('Succesfully redirects on map clinck',()=>{
+
+    })
+
+})
